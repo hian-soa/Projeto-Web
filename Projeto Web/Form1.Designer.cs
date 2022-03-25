d413c3de886bcf5474ecd722b57b2be584c7bf69
hian-soa <101405474+hian-soa@users.noreply.github.com>
2022-03-25 11:03:56 -0300

    test

--- Projeto Web/Form1.Designer.cs
+++ Projeto Web/Form1.Designer.cs
@@ -39,7 +39,6 @@ namespace Projeto_Web
             this.Seriesbtn = new System.Windows.Forms.Button();
             this.Filmesbtn = new System.Windows.Forms.Button();
             this.DropdownMenu = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -111,6 +110,7 @@ namespace Projeto_Web
             // Animebtn
             // 
             this.Animebtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Animebtn.ForeColor = System.Drawing.Color.Crimson;
             this.Animebtn.Location = new System.Drawing.Point(0, 105);
             this.Animebtn.Name = "Animebtn";
             this.Animebtn.Size = new System.Drawing.Size(154, 41);
@@ -147,21 +147,11 @@ namespace Projeto_Web
             this.DropdownMenu.Size = new System.Drawing.Size(154, 23);
             this.DropdownMenu.TabIndex = 2;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(278, 161);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(227, 112);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(834, 451);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
@@ -185,7 +175,6 @@ namespace Projeto_Web
         private System.Windows.Forms.Button Perfilbtn;
         private System.Windows.Forms.Button Amigosbtn;
         private System.Windows.Forms.TextBox Pesquisarbtn;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
     }
