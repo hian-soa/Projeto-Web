198d6adf0c125eb2b58528868af92dcb50bcf842
hian-soa <101405474+hian-soa@users.noreply.github.com>
2022-03-25 10:58:52 -0300

    ok

--- Projeto Web/Form1.Designer.cs
+++ Projeto Web/Form1.Designer.cs
@@ -39,6 +39,7 @@ namespace Projeto_Web
             this.Seriesbtn = new System.Windows.Forms.Button();
             this.Filmesbtn = new System.Windows.Forms.Button();
             this.DropdownMenu = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -146,11 +147,21 @@ namespace Projeto_Web
             this.DropdownMenu.Size = new System.Drawing.Size(154, 23);
             this.DropdownMenu.TabIndex = 2;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(278, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(227, 112);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(834, 451);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
